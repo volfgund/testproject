e69a8138e8f4d23436101d25af502b4094bb3497
volfgund <133907787+volfgund@users.noreply.github.com>
2023-06-28 22:05:59 +0300

    Add files via upload

---
+++ HW1/script.js
@@ -0,0 +1,24 @@
+// 1. String
+// const str = String(true);
+// const str = String(1000);
+// const str = String(null);
+//const str = String(undefined);
+//console.log(str)
+
+// 2. Number
+//const num = Number("123a");
+//const num = Number("1000");
+//const num = Number(null);
+//const num = Number(undefined);
+//const num = Number(false)
+//console.log(num)
+
+// 3.Boolean
+//const isBoolean = Boolean(null)
+//const isBoolean = Boolean(undefined)
+//const isBoolean = Boolean("")
+//const isBoolean = Boolean(0)
+//const isBoolean = Boolean(" ")
+//const isBoolean = Boolean(1000)
+const isBoolean = Boolean("1200")
+console.log(isBoolean)
